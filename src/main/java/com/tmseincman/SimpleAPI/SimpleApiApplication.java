9e305606a78bc268ca0ed042e2b0a9607a0c7c6f
tseincman <tseincman@gmail.com>
2023-07-18 15:01:03 -0300

    Student Class created

--- src/main/java/com/tmseincman/SimpleAPI/SimpleApiApplication.java
+++ src/main/java/com/tmseincman/SimpleAPI/SimpleApiApplication.java
@@ -1,9 +1,14 @@
 package com.tmseincman.SimpleAPI;
 
+import com.tmseincman.SimpleAPI.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +18,16 @@ public class SimpleApiApplication {
 		SpringApplication.run(SimpleApiApplication.class, args);
 	}
 	@GetMapping
-	public String hello(){
-		return "Hello, World";
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Maria",
+						"maria@gmail.com",
+						LocalDate.of(1990, Month.DECEMBER, 16),
+						32
+				)
+		);
 	}
 
 }
