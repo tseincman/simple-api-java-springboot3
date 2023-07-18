e62383cc0cfa4f655081ff7b6e7e222258cff72c
tseincman <tseincman@gmail.com>
2023-07-18 16:58:48 -0300

    Student Repository created

--- src/main/java/com/tmseincman/SimpleAPI/student/Student.java
+++ src/main/java/com/tmseincman/SimpleAPI/student/Student.java
@@ -1,13 +1,15 @@
 package com.tmseincman.SimpleAPI.student;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tb_student")
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/tmseincman/SimpleAPI/student/StudentService.java
+++ src/main/java/com/tmseincman/SimpleAPI/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.tmseincman.SimpleAPI.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Maria",
-                        "maria@gmail.com",
-                        LocalDate.of(1990, Month.DECEMBER, 16),
-                        32
-                )
-        );
+        return studentRepository.findAll();
     }
 }
