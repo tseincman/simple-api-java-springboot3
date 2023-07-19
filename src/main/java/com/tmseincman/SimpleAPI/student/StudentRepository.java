7e8e361477955a4f026b3bb979dcc6190d1c0fe2
tseincman <tseincman@gmail.com>
2023-07-19 10:14:32 -0300

    addNewStudent implemented

--- src/main/java/com/tmseincman/SimpleAPI/student/StudentRepository.java
+++ src/main/java/com/tmseincman/SimpleAPI/student/StudentRepository.java
@@ -1,8 +1,14 @@
 package com.tmseincman.SimpleAPI.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM tb_student WHERE email = :email")
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/tmseincman/SimpleAPI/student/StudentService.java
+++ src/main/java/com/tmseincman/SimpleAPI/student/StudentService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -19,5 +21,14 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+
     }
 }
